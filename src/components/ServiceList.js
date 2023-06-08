a10752815e7aad3923ba4932a5e8aea6eef4df8e
amandevopsreal <aman345kumarmishra@gmail.com>
2023-06-08 10:53:01 -0700

    test

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -5,7 +5,7 @@ const ServiceList = ({ services }) => {
     return (
         <div style={{marginBottom:"65px",marginTop:"65px"}}>
             {services.map((service, i) => {
-                return <Card link={services[i].link} service={services[i].service} />
+                return <Card key={i} link={services[i].link} service={services[i].service} />
             })}
         </div>
     );
