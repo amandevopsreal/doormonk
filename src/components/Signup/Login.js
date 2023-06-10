ac88a8f67b242b09d3a748bfb58e4fb1d31088fa
amandevopsreal <aman345kumarmishra@gmail.com>
2023-06-10 11:26:22 -0700

    some errors sorted out

--- src/components/Signup/Login.js
+++ src/components/Signup/Login.js
@@ -5,7 +5,7 @@ const Login = ({ onRouteChange }) => {
     return (
         <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 white fw6 ph0 mh0">Login</legend>
                         <div className="mt3">
@@ -23,7 +23,7 @@ const Login = ({ onRouteChange }) => {
                     <div className="lh-copy mt3">
                         <p onClick={()=>onRouteChange("signup")}className="pointer f6 white link dim black db">Sign up</p>
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
